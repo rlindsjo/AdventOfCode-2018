f044906c409503f6dd0c72d49869c43f88b77fd8
Roger Lindsjö <roger@tilialacus.net>
2018-12-08 15:08:51 +0100

    Use gradle 5 and test input loading

---
+++ src/test/java/net/tilialacus/adventofcode2018/InputTest.java
@@ -0,0 +1,17 @@
+package net.tilialacus.adventofcode2018;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputTest {
+
+    @Test
+    public void loadInput() {
+        List<String> lines = Input.inputFor(InputTest.class);
+        assertEquals(Arrays.asList("Line 1", "Line 2"), lines);
+    }
+}
