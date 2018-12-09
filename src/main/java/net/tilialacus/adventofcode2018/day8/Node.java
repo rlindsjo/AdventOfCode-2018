9d1c68293d5e059da21b82e47f334f5eb2d23adb
Roger Lindsjö <roger@tilialacus.net>
2018-12-09 11:18:55 +0100

    Day 8 part 1 sample solution

---
+++ src/main/java/net/tilialacus/adventofcode2018/day8/Node.java
@@ -0,0 +1,53 @@
+package net.tilialacus.adventofcode2018.day8;
+
+import java.util.Arrays;
+import java.util.PrimitiveIterator.OfInt;
+
+public class Node {
+    private final Node[] children;
+    private final int[] meta;
+
+    public Node(Node[] children, int[] meta) {
+        this.children = children;
+        this.meta = meta;
+    }
+
+    public static Node parse(String[] data) {
+        OfInt iterator = Arrays.stream(data).mapToInt(Integer::parseInt).iterator();
+        return build(iterator);
+    }
+
+    private static Node build(OfInt data) {
+        int childCount = data.nextInt();
+        int metaCount = data.nextInt();
+        return new Node(buildChildren(childCount, data), buildMeta(metaCount, data));
+    }
+
+    private static int[] buildMeta(int metaCount, OfInt data) {
+        int[] meta = new int[metaCount];
+        for (int i = 0; i < meta.length; i++) {
+            meta[i] = data.nextInt();
+        }
+        return meta;
+    }
+
+    private static Node[] buildChildren(int count, OfInt data) {
+        Node[] nodes = new Node[count];
+        for (int i = 0; i < nodes.length; i++) {
+            nodes[i] = build(data);
+        }
+        return nodes;
+    }
+
+    public Node[] getChildren() {
+        return children;
+    }
+
+    public int[] getMeta() {
+        return meta;
+    }
+
+    public int getMetaSum() {
+        return Arrays.stream(meta).sum() + Arrays.stream(children).mapToInt(Node::getMetaSum).sum();
+    }
+}
---
+++ src/test/java/net/tilialacus/adventofcode2018/day8/Day8Part1Test.java
@@ -0,0 +1,26 @@
+package net.tilialacus.adventofcode2018.day8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day8Part1Test {
+
+    private static final String sampleInput = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+
+    @Test
+    void parseInput() {
+        String[] data = sampleInput.split(" ");
+
+        assertEquals(16, data.length);
+
+        Node node = Node.parse(data);
+
+        assertEquals(2, node.getChildren().length);
+        assertEquals(3, node.getMeta().length);
+        assertArrayEquals(new int[] {1, 1, 2}, node.getMeta());
+
+        assertEquals(138, node.getMetaSum());
+    }
+}
